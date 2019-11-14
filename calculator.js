cd3999ab642ed6ddc01a2f11e95f56bd1f6ad5af
TrevorTaylor-TP <ttaylor@talentpath.com>
2019-11-14 13:21:42 -0600

    Added reverse polish notation function

--- calculator.js
+++ calculator.js
@@ -1,35 +1,95 @@
 class Calculator {
-	constructor(value) {
-		this.value = value;
-	}
+  constructor(value) {
+    this.value = value;
+  }
 
-	equals() {
-		return this.value;
-	}
+  equals() {
+    return this.value;
+  }
 
-	clear() {
-		this.value = 0;
-		return this;
-	}
+  clear() {
+    this.value = 0;
+    return this;
+  }
 
-	add(num) {
-		this.value += num;
-		return this;
-	}
+  add(num) {
+    this.value += num;
+    return this;
+  }
 
-	subtract(num) {
-		this.value -= num;
-		return this;
-	}
+  subtract(num) {
+    this.value -= num;
+    return this;
+  }
 
-	multiply(num) {
-		this.value *= num;
-		return this;
-	}
+  multiply(num) {
+    this.value *= num;
+    return this;
+  }
 
-	divide(num) {
-		this.value /= num;
-		return this;
-	}
+  divide(num) {
+    this.value /= num;
+    return this;
+  }
+  //support + _ * /
+  rpn(expression) {
+    let result = 0;
+    let stack = [];
+    let expArr = [];
+    if (expression === undefined || expression === null) {
+      return 0;
+    } else {
+      expArr = expression.split(" ");
+    }
+    for (const v of expArr) {
+      console.log(typeof Number.parseInt(v));
+      stack.push(v);
+      if (isNaN(parseInt(v))) {
+        stack.pop();
+        if (v === "+") {
+          //addition
+          let var2 = stack.pop();
+          let var1 = stack.pop();
+          let preResult = Number(var1) + Number(var2);
+
+          stack.push(preResult);
+        }
+
+        if (v === "-") {
+          //subtraction
+          let var2 = stack.pop();
+          let var1 = stack.pop();
+          let preResult = Number(var1) - Number(var2);
+
+          stack.push(preResult);
+        }
+
+        if (v === "*") {
+          //multiplication
+          let var2 = stack.pop();
+          let var1 = stack.pop();
+          let preResult = Number(var1) * Number(var2);
+
+          stack.push(preResult);
+        }
+
+        if (v === "/") {
+          //division
+          let var2 = stack.pop();
+          let var1 = stack.pop();
+          let preResult = Number(var1) / Number(var2);
+
+          stack.push(preResult);
+        }
+      }
+    }
+
+    if (stack.length > 1) {
+      return "Expression invalid (too many remaining items in stack)";
+    } else {
+      result = stack.pop();
+    }
+    return result;
+  }
 }
 module.exports = Calculator;
--- calculatorTest.test.js
+++ calculatorTest.test.js
@@ -2,33 +2,45 @@
 const Calculator = require("./calculator");
 
 describe("Calculator", () => {
-	test("has getter on `register` called `equals", () => {
-		const subject = new Calculator(5);
-		expect(subject.equals).toBeDefined();
-	});
+  test("has getter on `register` called `equals", () => {
+    const subject = new Calculator(5);
+    expect(subject.equals).toBeDefined();
+  });
 
-	test("has function called `add` that adds `number` to `register`", () => {
-		expect(new Calculator(5).add(3).equals()).toBe(8);
-	});
+  test("has function called `add` that adds `number` to `register`", () => {
+    expect(new Calculator(5).add(3).equals()).toBe(8);
+  });
 
-	test("has function called `subtract` that subtracts `number` from `register`", () => {
-		expect(new Calculator(5).subtract(3).equals()).toBe(2);
-	});
+  test("has function called `subtract` that subtracts `number` from `register`", () => {
+    expect(new Calculator(5).subtract(3).equals()).toBe(2);
+  });
 
-	test("has function called `multiply` that multiplies `number` with `register`", () => {
-		expect(new Calculator(5).multiply(3).equals()).toBe(15);
-	});
+  test("has function called `multiply` that multiplies `number` with `register`", () => {
+    expect(new Calculator(5).multiply(3).equals()).toBe(15);
+  });
 
-	test("has function called `divide` that divides `register` by `number`", () => {
-		expect(new Calculator(10).divide(2).equals()).toBe(5);
-	});
+  test("has function called `divide` that divides `register` by `number`", () => {
+    expect(new Calculator(10).divide(2).equals()).toBe(5);
+  });
 
-	test("has function called `clear` that clears the `register` setting it to `0`", () => {
-		expect(
-			new Calculator(5)
-				.clear()
-				.add(3)
-				.equals()
-		).toBe(3);
-	});
+  test("has function called `rpn` that evaluates an empty expression to `0`", () => {
+    expect(new Calculator().rpn()).toBe(0);
+  });
+
+  test("has function called `rpn` that evaluates an expression in reverse polish notation`", () => {
+    expect(new Calculator().rpn("1 2 +")).toBe(3);
+  });
+
+  test("has function called `rpn` that evaluates an expression in reverse polish notation`", () => {
+    expect(new Calculator().rpn("1 2 + 7 - 2 *")).toBe(-8);
+  });
+
+  test("has function called `clear` that clears the `register` setting it to `0`", () => {
+    expect(
+      new Calculator(5)
+        .clear()
+        .add(3)
+        .equals()
+    ).toBe(3);
+  });
 });
